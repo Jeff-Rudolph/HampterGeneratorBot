b224e61247782dbe63c494d4670a81e1d00c32ac
Jeff Rudolph <jeffrey.david.rudolph@gmail.com>
2023-03-31 09:54:07 -0400

    Infamous "Lober" update

--- bot.py
+++ bot.py
@@ -29,16 +29,16 @@ async def ascii(ctx):
 @bot.command
 @lightbulb.command('hampter','Generates Hampter Image with AI',auto_defer=True)
 @lightbulb.implements(lightbulb.SlashCommand)
-async def generate_image(ctx):
+async def generate_hampter_image(ctx):
  
     response = await openai_async.generate_img(
      config.openAI_key,
      timeout=8,
      payload={
-         "prompt": randomInput(),
+         "prompt": randomHampterInput(),
          "n": 1,
          "size": "512x512"
-        },
+        }
     )
     image_url = response.json()["data"][0]["url"]
 
@@ -46,11 +46,32 @@ async def generate_image(ctx):
     await ctx.respond(attachment=image_url)
 
 
-def randomInput():
+@bot.command
+@lightbulb.command('lober', 'Generates Lober Image with AI',auto_defer=True)
+@lightbulb.implements(lightbulb.SlashCommand)
+async def generate_lober_image(ctx):
+
+    response = await openai_async.generate_img(
+        config.openAI_key,
+        timeout=8,
+        payload={
+         "prompt": randomLoberInput(),
+         "n": 1,
+         "size": "512x512"
+        }
+    )
+    image_url = response.json()["data"][0]["url"]
+
+    await ctx.respond(attachment=image_url)
+
+
+def randomHampterInput():
     str_list = ["Hampter Meme", "Dank Hampter Meme", "Very Funny Hampter Meme", "Cursed Deep Fried Hampter Meme"]
     return str_list[random.randrange(0,len(str_list))]
     
 
-
+def randomLoberInput():
+    str_list = ["Lobster Meme", "Dank Lobster Meme", "Meme featuring Larry the Lobster from the show Spongebob", "Blue Lobster Meme", "Cursed Lobster Meme", "Renaissance Painting of a Lobster", "Portrait painting of a Lobster wearing clothes"]
+    return str_list[random.randrange(0,len(str_list))]
 
 bot.run()
