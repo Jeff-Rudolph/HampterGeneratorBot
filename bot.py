0dc888e5d9c7368dbe3f820cf379de33b70e0bfe
Jeff Rudolph <jeffrey.david.rudolph@gmail.com>
2023-03-31 12:08:14 -0400

    Lober Present, removed ascii

--- bot.py
+++ bot.py
@@ -1,13 +1,8 @@
-import aiohttp
-import aiofiles
 import hikari
 import lightbulb
-import openai
 import random
 import config
-from  PIL import Image
 import openai_async
-
 
 
 bot = lightbulb.BotApp(token=config.bot_token)
@@ -18,27 +13,20 @@ async def bot_started(event):
     print('HampterGenerator is online.')
 
 
-@bot.command
-@lightbulb.command('ascii','ascii hampter')
-@lightbulb.implements(lightbulb.SlashCommand)
-async def ascii(ctx):
-    
-    await ctx.respond('pong!')
-
 
 @bot.command
 @lightbulb.command('hampter','Generates Hampter Image with AI',auto_defer=True)
 @lightbulb.implements(lightbulb.SlashCommand)
-async def generate_image(ctx):
+async def generate_hampter_image(ctx):
  
     response = await openai_async.generate_img(
      config.openAI_key,
      timeout=8,
      payload={
-         "prompt": randomInput(),
+         "prompt": randomHampterInput(),
          "n": 1,
          "size": "512x512"
-        },
+        }
     )
     image_url = response.json()["data"][0]["url"]
 
@@ -46,11 +34,33 @@ async def generate_image(ctx):
     await ctx.respond(attachment=image_url)
 
 
-def randomInput():
+@bot.command
+@lightbulb.command('lober', 'Generates Lober Image with AI',auto_defer=True)
+@lightbulb.implements(lightbulb.SlashCommand)
+async def generate_lober_image(ctx):
+
+    response = await openai_async.generate_img(
+        config.openAI_key,
+        timeout=8,
+        payload={
+         "prompt": randomLoberInput(),
+         "n": 1,
+         "size": "512x512"
+        }
+    )
+    image_url = response.json()["data"][0]["url"]
+
+    await ctx.respond(attachment=image_url)
+
+
+
+def randomHampterInput():
     str_list = ["Hampter Meme", "Dank Hampter Meme", "Very Funny Hampter Meme", "Cursed Deep Fried Hampter Meme"]
     return str_list[random.randrange(0,len(str_list))]
     
 
-
+def randomLoberInput():
+    str_list = ["Lobster Meme", "Dank Lobster Meme", "Meme featuring Larry the Lobster from the show Spongebob", "Blue Lobster Meme", "Cursed Lobster Meme", "Portrait painting of a Lobster wearing clothes"]
+    return str_list[random.randrange(0,len(str_list))]
 
 bot.run()
