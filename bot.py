028efdd6101ae1be5d380022a4ab03880c496fb0
Jeff Rudolph <jeffrey.david.rudolph@gmail.com>
2023-05-01 13:24:50 -0400

    got rid of camelCase

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ async def bot_started(event):
 @lightbulb.implements(lightbulb.SlashCommand)
 async def generate_hampter_image(ctx):
  
-    response = await respond(randomHampterInput())
+    response = await respond(random_hampter_input())
     image_url = response.json()["data"][0]["url"]
     await ctx.respond(attachment=image_url)
 
@@ -27,7 +27,7 @@ async def generate_hampter_image(ctx):
 @lightbulb.implements(lightbulb.SlashCommand)
 async def generate_asoingbob_image(ctx):
 
-    response = await respond(randomAsoingbobInput())
+    response = await respond(random_asoingbob_input())
     image_url = response.json()["data"][0]["url"]
     await ctx.respond(attachment=image_url)
 
@@ -37,21 +37,21 @@ async def generate_asoingbob_image(ctx):
 @lightbulb.implements(lightbulb.SlashCommand)
 async def generate_lober_image(ctx):
 
-    response = await respond(randomLoberInput())
+    response = await respond(random_lober_input())
     image_url = response.json()["data"][0]["url"]
     await ctx.respond(attachment=image_url)
 
 
 #I apologize to anyone who has to read these prompts lol.
-def randomHampterInput():
+def random_hampter_input():
     str_list = ["Hampter Meme", "Dank Hampter Meme", "Very Funny Hampter Meme", "Cursed Deep Fried Hampter Meme"]
     return str_list[random.randrange(0,len(str_list))]
 
-def randomAsoingbobInput():
+def random_asoingbob_input():
     str_list = ["Spongebob Meme", "Dank Mr. Krabz Meme", "Dark And Scary Squidward Meme", "Cursed Deep Fried Meme Featuring Characters From Spongebob", "Generate An Image From The TV Show Spongebob In Gross-Up Close-Up Style"]
     return str_list[random.randrange(0,len(str_list))]
     
-def randomLoberInput():
+def random_lober_input():
     str_list = ["Lobster Meme", "Dank Lobster Meme", "Meme featuring Larry the Lobster from the show Spongebob", "Blue Lobster Meme", "Cursed Lobster Meme", "Portrait painting of a Lobster wearing clothes"]
     return str_list[random.randrange(0,len(str_list))]
 
